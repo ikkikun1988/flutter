578378291d4746bead8b02c400cf144950d4db37
Joe Domingo <jdomingo.etraid@grupoetra.com>
2026-03-26 17:01:55 +0100

    hello world

---
+++ 01-DART INTRO/01_hello_world.dart
@@ -0,0 +1,12 @@
+void main() {
+  //var myName = 'Jose';
+  //String myName = 'Jose Domingo';
+  //late final myName;  
+ // myName='mundo';
+  const myName = 'Jose Domingo';
+
+  
+  print('hola $myName');
+  print('hola ${myName.toUpperCase()}');
+  print('Hola ${1+1}');
+}
